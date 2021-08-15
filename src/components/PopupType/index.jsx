283c91f52b7ba440501ee0160d2df2c508f9bd13
ldl <739316878@qq.com>
2021-08-15 12:53:21 +0800

    弹窗

---
+++ src/components/PopupType/index.jsx
@@ -0,0 +1,70 @@
+import React, {useState,forwardRef, useEffect} from 'react';
+import { Popup, Icon } from "zarm";
+import cx from "classnames";
+import s from "./index.module.less";
+import { get } from '@/utils'
+const PopupType = forwardRef(({selectedType},ref) => {
+  const [isShow, setIsShow] = useState(false);
+  const [currentType, setCurrentType] = useState({name:'全部类型',id:'all'});
+  const [expenseTypeArr, setExpenseTypeArr] = useState([]);
+  const [incomeTypeArr, setIncomeTypeArr] = useState([]);
+  useEffect(async () => {
+    // 请求标签接口放在弹窗内，这个弹窗可能会被复用，所以请求如果放在外面，会造成代码冗余。
+    const { data: { list } } = await get('/api/type/list')
+    setExpenseTypeArr(list.filter(i => i.type == 1))
+    setIncomeTypeArr(list.filter(i => i.type == 2))
+  }, []);
+
+  if (ref) {
+    ref.current = {
+      show: () => {
+        setIsShow(true);
+      },
+      close: () => {
+        setIsShow(false);
+      }
+    }
+  };
+  const chooseType = (param) => {
+    setCurrentType(param);
+    setIsShow(false);
+    selectedType(param);
+  }
+  return (
+    <Popup
+        visible={isShow}
+        direction="bottom"
+        onMaskClick={() => setIsShow(false)}
+        destroy={false}
+        mountContainer={() => document.body}
+      >
+      <div className={s.popupTypeContent}>
+        <div className={s.header}>请选择类型<Icon type="wrong" className={s.close} onClick={() => setIsShow(false)} /></div>
+        <div className={s.content}>
+          <div className={cx({ [s.typeItem]: true, [s.active]: currentType.id == 'all' })} onClick={()=>chooseType({id:'all',name:'全部类型'})}>全部类型</div>
+          <div style={{marginBottom:10}}>支出</div>
+          <div className={s.typeWrap}>
+            {
+              expenseTypeArr.map((item)=>{
+                return (
+                  <p className={cx({ [s.typeItem]: true, [s.active]: currentType.id == item.id })} onClick={()=>chooseType(item)}>{item.name}</p>
+                )
+              })
+            }
+          </div>
+          <div style={{marginBottom:10}}>收入</div>
+          <div className={s.typeWrap}>
+            {
+              incomeTypeArr.map((item)=>{
+                return (
+                  <p className={cx({ [s.typeItem]: true, [s.active]: currentType.id == item.id })} onClick={()=>chooseType(item)}>{item.name}</p>
+                )
+              })
+            }
+          </div>
+        </div>
+      </div>
+    </Popup>
+  )
+})
+export default PopupType;
--- src/container/Home/index.jsx
+++ src/container/Home/index.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
-import { Button } from "zarm";
-import s from "./test.module.less";
+import React, { useState, useRef } from 'react';
+import { Button, Icon } from "zarm";
+import dayjs from "dayjs";
+import s from "./index.module.less";
+import PopupType from "@/components/PopupType";
+import PopupDate from "@/components/PopupDate";
 const Home = () => {
+  const [totalExpense, setTotalExpense] = useState(0);
+  const [totalIncom, setTotalIncom] = useState(0);
+  const [selectType, setSelectType] = useState({name:'全部类型',id:'all'});
+  const [selectDate, setSelectDate] = useState(dayjs().format('YYYY-MM'));
+  const typeRef = useRef();
+  const dateRef = useRef();
+  const toggleDate = () => {
+    dateRef.current.show();
+  };
+  const toggleType = () => {
+    typeRef.current.show();
+  };
+  const selectedType = (param) => {
+    setSelectType(param);
+  };
+  const onSelect = (param) => {
+    setSelectDate(param);
+  }
   return (
     <div className={s.index}>
-      首页
+      <div className={s.header}>
+        <div className={s.headerTop}>
+          <div className={s.expense}>总支出：<span>¥{totalExpense}</span></div>
+          <div className={s.income}>总收入：<span>¥{totalIncom}</span></div>
+        </div>
+        <div className={s.headerBottom}>
+          <div className={s.date} onClick={toggleDate}>{selectDate}<Icon className={s.arrow} type="arrow-bottom" /></div>
+          <div className={s.type} onClick={toggleType}>{selectType.name}<Icon className={s.arrow} type="arrow-bottom" /></div>
+        </div>
+      </div>
+      <PopupType ref={typeRef} selectedType={selectedType}/>
+      <PopupDate ref={dateRef} onSelect={onSelect} mode={'month'}/>
     </div>
   )
 }
